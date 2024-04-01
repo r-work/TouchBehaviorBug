b137acf00e324283c781359adf938d474c218ccb
Rebin <rebinf@outlook.com>
2024-04-01 12:15:23 +0100

    bug repro

---
+++ TouchBehaviorBug/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace TouchBehaviorBug
+{
+    public partial class MainPageViewModel
+    {
+        public List<Item> Items { get; set; } = new List<Item>()
+        {
+            new Item { Name = "Item 1", Description = "This is item 1" },
+            new Item { Name = "Item 2", Description = "This is item 2" },
+        };
+
+        [RelayCommand]
+        public void ItemClicked(Item item)
+        {
+        }
+    }
+}
