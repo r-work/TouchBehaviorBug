37ad5e62fc9902cc7a7d5322f2553af7662ae350
Rebin <rebinf@outlook.com>
2024-04-01 12:15:11 +0100

    add toolkit

--- TouchBehaviorBug/MauiProgram.cs
+++ TouchBehaviorBug/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace TouchBehaviorBug
 {
@@ -9,6 +10,7 @@ namespace TouchBehaviorBug
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -16,7 +18,7 @@ namespace TouchBehaviorBug
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
