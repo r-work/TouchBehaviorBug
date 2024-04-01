f4b1b959bc50a162afbf9f917a05dde121244afe
Rebin <rebinf@outlook.com>
2024-04-01 12:03:16 +0100

    Add project files.

---
+++ TouchBehaviorBug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TouchBehaviorBug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TouchBehaviorBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TouchBehaviorBug
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
